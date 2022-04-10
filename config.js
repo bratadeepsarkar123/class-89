5d70f96f9a55c7e24ed59cb46239b3d7459d9532
bratadeepsarkar123 <79410839+bratadeepsarkar123@users.noreply.github.com>
2022-04-10 19:01:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+    /*apiKey: "AIzaSyAlhxmFuVDRGJjvkFPSS5Q3PCyMiEFxJPw",
+    authDomain: "storytellin-72416.firebaseapp.com",
+    projectId: "storytellin-72416",
+    storageBucket: "storytellin-72416.appspot.com",
+    messagingSenderId: "993908358863",
+    appId: "1:993908358863:web:63708cad49a4b945ba41ba"*/
+    apiKey: "AIzaSyBxuskQvDjjOcdRuQHgJXcZAiuBmcYUwxU",
+    authDomain: "storytelling-app-24bfa.firebaseapp.com",
+    databaseURL: "https://storytelling-app-24bfa-default-rtdb.firebaseio.com",
+    projectId: "storytelling-app-24bfa",
+    storageBucket: "storytelling-app-24bfa.appspot.com",
+    messagingSenderId: "1098934757643",
+    appId: "1:1098934757643:web:dfed1bee62b2af69fe5a0e"
+  };
